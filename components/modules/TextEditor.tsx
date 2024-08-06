477f9b0055e8cc657af7d219b03caef83de90220
JeonGAMJA <indian_potato@naver.com>
2024-08-06 23:57:23 +0900

    refactor: 불필요한 import 삭제

--- components/modules/TextEditor.tsx
+++ components/modules/TextEditor.tsx
@@ -1,47 +1,41 @@
-'use client'
+"use client";
 
-import { useRef,useState } from 'react'
-import dynamic from 'next/dynamic';
-import Editor from 'quill/core/editor';
-import Quill from 'quill';
+import { useRef, useState } from "react";
+import Quill from "quill";
 
-const Delta = Quill.import('delta');
+const Delta = Quill.import("delta");
 
 const TextEditor = () => {
-    const [range, setRange] = useState();
-    const [lastChange, setLastChange] = useState();
-    const [readOnly, setReadOnly] = useState(false);
-  
+  const [range, setRange] = useState();
+  const [lastChange, setLastChange] = useState();
+  const [readOnly, setReadOnly] = useState(false);
 
-    const quillRef = useRef();
-  
-    return (
-      <div>
-        <div class="controls">
-          <label>
-            Read Only:{' '}
-            
-          </label>
-          <button
-            className="controls-right"
-            type="button"
-            onClick={() => {
-              alert(quillRef.current?.getLength());
-            }}
-          >
-            Get Content Length
-          </button>
-        </div>
-        <div className="state">
-          <div className="state-title">Current Range:</div>
-          {range ? JSON.stringify(range) : 'Empty'}
-        </div>
-        <div className="state">
-          <div className="state-title">Last Change:</div>
-          {lastChange ? JSON.stringify(lastChange.ops) : 'Empty'}
-        </div>
+  const quillRef = useRef();
+
+  return (
+    <div>
+      <div class="controls">
+        <label>Read Only: </label>
+        <button
+          className="controls-right"
+          type="button"
+          onClick={() => {
+            alert(quillRef.current?.getLength());
+          }}
+        >
+          Get Content Length
+        </button>
       </div>
-    );
-}
+      <div className="state">
+        <div className="state-title">Current Range:</div>
+        {range ? JSON.stringify(range) : "Empty"}
+      </div>
+      <div className="state">
+        <div className="state-title">Last Change:</div>
+        {lastChange ? JSON.stringify(lastChange.ops) : "Empty"}
+      </div>
+    </div>
+  );
+};
 
-export default TextEditor
+export default TextEditor;
